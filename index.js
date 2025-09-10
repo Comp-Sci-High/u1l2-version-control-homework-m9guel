6fd65e148b6044b51d2d65224ce935adbbd14759
m9guel <miguel.sanchez26@compscihigh.org>
2025-09-10 05:16:54 +0000

    added the steps to pushing changes in hit into an array with each step being a string

--- index.js
+++ index.js
@@ -40,7 +40,11 @@ console.log(versionControlBenefits);
 
 // 4. Explain the steps to pushing changes in Git.
 // Write your answer as an array of steps below, with each step being a string.
-let stepsToPushChanges = [];
+let stepsToPushChanges = 
+["1: git add .",
+ "2: git commit -m message",
+ "3: git push"  
+ ];
 
 console.log("What are the steps to pushing changes in Git?");
 console.log(stepsToPushChanges); 
